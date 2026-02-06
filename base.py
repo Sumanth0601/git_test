e97114dd820966a996484f8152f0e1bc6572fe4a
T Sumanth <t.sumanth@cimpress.com>
2026-02-06 23:11:57 +0530

    one more line

--- base.py
+++ base.py
@@ -4,6 +4,7 @@ print("Hello, World!")
 print('and here i add few more lines to test git yay')
 
 print("and i do something else in the same line")
+print('and the next line as well')
 a = 10
 b = 20 
 print(a + b)
